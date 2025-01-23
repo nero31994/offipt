1ab1a783c70b429fd5921e60c0926bd086a40a21
nero31994 <arenaofvalorph937@gmail.com>
2025-01-23 12:34:18 +0800

    Create remote.js

---
+++ remote.js
@@ -0,0 +1,10 @@
+// This script is for adding TV remote support
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'ArrowUp') {
+    // Handle 'up' arrow key press
+  } else if (event.key === 'ArrowDown') {
+    // Handle 'down' arrow key press
+  } else if (event.key === 'Enter') {
+    // Handle 'enter' key press to select a channel
+  }
+});

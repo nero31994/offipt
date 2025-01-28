7aa36eebe36d2ea8c5e6ddd72a8933237db7bd17
nero31994 <arenaofvalorph937@gmail.com>
2025-01-28 14:48:46 +0800

    Update channels.js

--- channels.js
+++ channels.js
@@ -1,14 +1,10 @@
+// Define the channels in this separate file
 const channels = [
-  {
-    name: "Channel 1 (DRM)",
-    stream: "https://path/to/encrypted/stream1.mpd",
-    keyId: "d72d9c36b8504f5d944da174b8f9e3ba",
-    key: "bfc4169b0f4bc875a94f39b4f1cf9de5"
-  },
-  {
-    name: "Channel 2 (DRM)",
-    stream: "https://path/to/encrypted/stream2.mpd",
-    keyId: "4e0c3ac61c634234b12e6b1c6a209a7e",
-    key: "662f8cc3fc2c4b979f8090da953b4073"
-  }
+  { name: "TVUP", src: "https://qp-pldt-live-grp-09-prod.akamaized.net/out/u/tvup_prd.mpd", drm: "clearkey", key: "83e813ccd4ca4837afd611037af02f63:a97c515dbcb5dcbc432bbd09d15afd41" },
+  { name: "UAAP VARSITY", src: "https://qp-pldt-live-grp-04-prod.akamaized.net/out/u/cg_uaap_cplay_sd.mpd", drm: "clearkey", key: "95588338ee37423e99358a6d431324b9:6e0f50a12f36599a55073868f814e81e" },
+  { name: "IBC", src: "https://qp-pldt-live-grp-07-prod.akamaized.net/out/u/ibc13_sd.mpd", drm: "clearkey", key: "04e292bc99bd4ccba89e778651914254:ff0a62bdf8920ce453fe680330b563a5" },
+  { name: "PINOY BOX OFFICE", src: "https://qp-pldt-live-grp-04-prod.akamaized.net/out/u/pbo_sd.mpd", drm: "clearkey", key: "dcbdaaa6662d4188bdf97f9f0ca5e830:31e752b441bd2972f2b98a4b1bc1c7a1" },
+  { name: "VIVACINEMA", src: "https://qp-pldt-live-grp-06-prod.akamaized.net/out/u/viva_sd.mpd", drm: "clearkey", key: "07aa813bf2c147748046edd930f7736e:3bd6688b8b44e96201e753224adfc8fb" },
+  { name: "SKY NEWS", src: "https://linear417-gb-hls1-prd-ak.cdn.skycdp.com/100e/Content/HLS_001_1080_30/Live/channel(skynews)/index_1080-30.m3u8", drm: "" },
+  { name: "AL JAZEERA", src: "https://d1cy85syyhvqz5.cloudfront.net/v1/master/7b67fbda7ab859400a821e9aa0deda20ab7ca3d2/aljazeeraLive/AJE/index.m3u8", drm: "" },
 ];
